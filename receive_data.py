96160613c9217b2b6804bfdefc4a9752ba338156
Levy Nunes <levy.vix@gmail.com>
2023-08-20 15:42:51 -0300

    load dot env

--- receive_data.py
+++ receive_data.py
@@ -1,44 +1,43 @@
 import asyncio
-
 from azure.eventhub.aio import EventHubConsumerClient
-from azure.eventhub.extensions.checkpointstoreblobaio import (
-    BlobCheckpointStore,
-)
+from azure.eventhub.extensions.checkpointstoreblobaio import BlobCheckpointStore
+from dotenv import dotenv_values
 
+# Load environment variables
+config = dotenv_values(".env")
 
-import dotenv
-import os
+# Get environment variables
+STORAGE_CONNECTION_STR = config["BLOB_STORAGE_CONNECTION_STRING"]
+BLOB_CONTAINER_NAME = config["BLOB_CONTAINER_NAME"]
+CONNECTION_STR = config["EVENT_HUB_CONNECTION_STR"]
+EVENTHUB_NAME = config["EVENT_HUB_NAME"]
 
-dotenv.load_dotenv(dotenv.find_dotenv())
-
-STORAGE_CONNECTION_STR = os.getenv("BLOB_STORAGE_CONNECTION_STRING")
-BLOB_CONTAINER_NAME = os.getenv("BLOB_CONTAINER_NAME")
-CONNECTION_STR = os.getenv("EVENT_HUB_CONNECTION_STR")
-EVENTHUB_NAME = os.getenv("EVENT_HUB_NAME")
-
-
+# Event handler function
 async def on_event(partition_context, event):
-    # Put your code here.
-    # Do some sync or async operations. If the operation is i/o intensive,
-    # async will have better performance.
+    # Print the event data
     print(event)
-    # print(event.body_as_str(encoding='UTF-8'))
+    # Update the checkpoint so that the program doesn't read the event again in case of a restart
     await partition_context.update_checkpoint(event)
 
-
+# Main function
 async def main(client):
+    # Start the client and receive events
     async with client:
         await client.receive(on_event)
 
+# Entry point of the program
 if __name__ == '__main__':
+    # Create a checkpoint store using Blob storage
     checkpoint_store = BlobCheckpointStore.from_connection_string(
         STORAGE_CONNECTION_STR,
         container_name=BLOB_CONTAINER_NAME,
     )
+    # Create an Event Hub consumer client
     client = EventHubConsumerClient.from_connection_string(
         CONNECTION_STR,
         consumer_group='$Default',
         eventhub_name=EVENTHUB_NAME,
         checkpoint_store=checkpoint_store
     )
+    # Run the main function
     asyncio.run(main(client))
