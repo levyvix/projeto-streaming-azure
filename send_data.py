fc0beb5fb2d435536dada6c7da8cc4e3df25bf03
Levy Nunes <levy.vix@gmail.com>
2023-08-20 15:39:05 -0300

    clean and add comments

--- send_data.py
+++ send_data.py
@@ -1,23 +1,25 @@
 import asyncio
 from azure.eventhub import EventData
 from azure.eventhub.aio import EventHubProducerClient
-
 import dotenv
 import os
-
 import random
 
+# Load environment variables
 dotenv.load_dotenv(dotenv.find_dotenv())
 
+# Generate event data
 event_data = [{
     "CodCAM": "CM" + str(i).zfill(3),
     "Temperatura": random.randint(-10, 5),
     "Odometro": random.randint(0, 100),
 } for i in range(50)]
 
+# Get environment variables for event hub connection
 EVENT_HUB_CONNECTION_STR = os.getenv("EVENT_HUB_CONNECTION_STR")
 EVENT_HUB_NAME = os.getenv("EVENT_HUB_NAME")
 
+# Define asynchronous function to send data to event hub
 async def run():
     # Create a producer client to send messages to the event hub.
     # Specify a connection string to your event hubs namespace and
@@ -27,21 +29,18 @@ async def run():
         eventhub_name=EVENT_HUB_NAME
     )
 
+    # Start the producer client
     async with producer:
         # Create a batch.
         event_data_batch = await producer.create_batch()
 
         # Add events to the batch.
-        # event_data_batch.add(EventData("First event"))
-        # event_data_batch.add(EventData("Second event"))
-        # event_data_batch.add(EventData("Third event"))
-
         for event in event_data:
             event_data_batch.add(EventData(str(event)))
-
 
         # Send the batch of events to the event hub.
         await producer.send_batch(event_data_batch)
         print(f"Sent a batch of {len(event_data)} events to the event hub.")
 
+# Run the function
 asyncio.run(run())
